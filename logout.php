0c33dcfb889d6719d151447cc68099e7c9dbd286
Dineth Nethmika <pkdnethmika@students.nsbm.ac.lk>
2025-09-17 13:43:56 +0530

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-session_unset();   // remove all admin session variables
+session_unset();   // remove all session variables
 session_destroy(); // destroy session
-header("Location: index.php"); // back to admin login page
+header("Location: index.php");
 exit;
